c6f1374bb30ec4b168554c928920fece7933c0c6
Liam Nelski <ljnelski@gmail.com>
2020-10-01 20:41:47 -0400

    Created 2nd Level and added a 2 camera scripts

---
+++ Platformer/Assets/Scripts/CameraControllerWithBuffer.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControllerWithBuffer : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+
+    [Range(1.0f, 10f)][SerializeField] private float cameraOffSetX = 0.5f;
+    [Range(1.0f, 10f)] [SerializeField] private float cameraOffSetY = 0.5f;
+  
+    // Update is called once per frame
+    void Update()
+    {
+        //check the x threshold
+        if (player.position.x < transform.position.x - (0.5f * cameraOffSetX)) // Left
+        {
+            transform.position = new Vector3(
+                player.position.x + (0.5f * cameraOffSetX),
+                transform.position.y,
+                transform.position.z);
+        }
+        else if (player.position.x > transform.position.x + (0.5f * cameraOffSetX)) // Right
+        {
+            transform.position = new Vector3(
+                player.position.x - (0.5f * cameraOffSetX),
+                transform.position.y,
+                transform.position.z);
+        }
+
+        //check the y threshold
+        if (player.position.y < transform.position.y - (0.5f * cameraOffSetY)) // up
+        {
+            transform.position = new Vector3(
+                transform.position.x ,
+                player.position.y + (0.5f * cameraOffSetY),
+                transform.position.z);
+        }
+        else if (player.position.y > transform.position.y + (0.5f * cameraOffSetY)) // down
+        {
+            transform.position = new Vector3(
+                transform.position.x ,
+                player.position.y - (0.5f * cameraOffSetY),
+                transform.position.z);
+        }
+
+
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireCube(transform.position, new Vector3(cameraOffSetX, cameraOffSetY, 0f));
+    }
+}
---
+++ Platformer/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //"Public Varibles"
+    [SerializeField] private float speed = 10.0f;
+    [SerializeField] private float jumpForce = 500.0f;
+    [SerializeField] private float groundCheckRadius = 0.15f;
+    [SerializeField] private Transform groundCheckPos;
+    [SerializeField] private LayerMask whatIsGround;
+
+    //Private Variables
+    private Rigidbody2D rbody;
+    private bool isGrounded = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        rbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Physics
+    void FixedUpdate()
+    {
+        
+        float horiz = Input.GetAxis("Horizontal");
+        isGrounded = GroundCheck();
+
+        //jump code
+        if (isGrounded && Input.GetAxis("Jump") > 0)
+        {
+            rbody.AddForce(new Vector2(0.0f, jumpForce));
+            isGrounded = false;
+        }
+
+        //Move code        
+        rbody.velocity = new Vector2(horiz * speed, rbody.velocity.y);    
+
+
+    }
+    private bool GroundCheck()
+    {
+        return Physics2D.OverlapCircle(groundCheckPos.position, groundCheckRadius, whatIsGround);
+    }
+}
