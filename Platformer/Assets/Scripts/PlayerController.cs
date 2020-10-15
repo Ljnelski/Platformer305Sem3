57515724c1e53a5c08b8d575b18e6c3bf0cec454
Liam Nelski <ljnelski@gmail.com>
2020-10-15 10:10:44 -0400

    Added a bunch of animations

--- Platformer/Assets/Scripts/PlayerController.cs
+++ Platformer/Assets/Scripts/PlayerController.cs
@@ -14,18 +14,21 @@ public class PlayerController : MonoBehaviour
     //Private Variables
     private Rigidbody2D rbody;
     private bool isGrounded = false;
+    private Animator anim;
+    private bool isFaceingRight = true;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
         rbody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Physics
     void FixedUpdate()
     {
-        
+
         float horiz = Input.GetAxis("Horizontal");
         isGrounded = GroundCheck();
 
@@ -36,13 +39,31 @@ public class PlayerController : MonoBehaviour
             isGrounded = false;
         }
 
-        //Move code        
-        rbody.velocity = new Vector2(horiz * speed, rbody.velocity.y);    
+        //Move code              
+        rbody.velocity = new Vector2(horiz * speed, rbody.velocity.y);
 
+        //check if sprite needs to be fliped
+        if (isFaceingRight && rbody.velocity.x < 0.0f || !isFaceingRight && rbody.velocity.x > 0.0f)        
+            Flip();
+        
+        
 
+        // Communicate with anim
+        anim.SetFloat("xVelocity", Mathf.Abs(rbody.velocity.x));
+        anim.SetFloat("yVelocity", rbody.velocity.y);
+        anim.SetBool("isGrounded", isGrounded);
+               
     }
     private bool GroundCheck()
     {
         return Physics2D.OverlapCircle(groundCheckPos.position, groundCheckRadius, whatIsGround);
+    }
+    private void Flip()
+    {
+        Vector3 temp = transform.localScale;
+        temp.x *= -1;
+        transform.localScale = temp;
+
+        isFaceingRight = !isFaceingRight;
     }
 }
